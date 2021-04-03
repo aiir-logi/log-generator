5ff9a8c6d3d3ad18e283674b5da928c42d733a2d
Adam-Gajda <248877@student.pwr.edu.pl>
2021-04-03 14:14:03 +0200

    initial commit

---
+++ src/main/java/com/example/loggenerator/LoggeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.loggenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggeneratorApplication.class, args);
+    }
+
+}
