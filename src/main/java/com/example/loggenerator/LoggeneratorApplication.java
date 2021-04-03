478a121d25ccdcca7ca13f8b1a0d2d5ae7d8802e
Adam-Gajda <248877@student.pwr.edu.pl>
2021-04-03 14:21:29 +0200

    Add example logger

--- src/main/java/com/example/loggenerator/LoggeneratorApplication.java
+++ src/main/java/com/example/loggenerator/LoggeneratorApplication.java
@@ -1,12 +1,16 @@
 package com.example.loggenerator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoggeneratorApplication {
-
-    public static void main(String[] args) {
+    private static Logger logger = LogManager.getLogger(LoggeneratorApplication.class);
+    public static void main(String[] args)
+    {
+        logger.error("Logger works");
         SpringApplication.run(LoggeneratorApplication.class, args);
     }
 
