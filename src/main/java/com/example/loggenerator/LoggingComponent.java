729f33900556b06f9d9bbcb475aa536cf0baf76a
Ksawery Karwacki <ksawery.karwacki@aptitudesoftware.com>
2021-06-11 13:42:34 +0200

    Fixed logger to work with kafka

--- src/main/java/com/example/loggenerator/LoggingComponent.java
+++ src/main/java/com/example/loggenerator/LoggingComponent.java
@@ -1,5 +1,7 @@
 package com.example.loggenerator;
 
+import com.thedeanda.lorem.Lorem;
+import com.thedeanda.lorem.LoremIpsum;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,10 +13,15 @@ public class LoggingComponent {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
+    private Lorem lorem = LoremIpsum.getInstance();
+
     @Scheduled(fixedDelay = 1000L)
     void run() throws InterruptedException {
         while(true) {
-            logger.warn("test");
+            logger.warn(lorem.getWords(5, 10));
+            logger.info(lorem.getWords(2, 5));
+            logger.error(lorem.getWords(7, 10));
+            logger.trace(lorem.getWords(10, 15));
             Thread.sleep(500);
         }
     }
