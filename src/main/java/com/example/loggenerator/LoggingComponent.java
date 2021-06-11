b408cd662dfaac0521c3c3d36e7e32baac37d0be
Ksawery Karwacki <ksawery.karwacki@aptitudesoftware.com>
2021-06-11 13:56:11 +0200

    Log generator set to 10 threads for speedup generation of logs

--- src/main/java/com/example/loggenerator/LoggingComponent.java
+++ src/main/java/com/example/loggenerator/LoggingComponent.java
@@ -2,10 +2,14 @@ package com.example.loggenerator;
 
 import com.thedeanda.lorem.Lorem;
 import com.thedeanda.lorem.LoremIpsum;
+import lombok.SneakyThrows;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 @Component
@@ -16,7 +20,15 @@ public class LoggingComponent {
     private Lorem lorem = LoremIpsum.getInstance();
 
     @Scheduled(fixedDelay = 1000L)
-    void run() throws InterruptedException {
+    void run() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for(int i=0; i<10; i++) {
+            executorService.submit(this::log);
+        }
+    }
+
+    @SneakyThrows
+    void log() {
         while(true) {
             logger.warn(lorem.getWords(5, 10));
             logger.info(lorem.getWords(2, 5));
